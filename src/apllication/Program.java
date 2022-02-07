a8a196913eddad9b587dd67ea96d55eda9e65829
thiagopazjs <thiagopaz907@gmail.com>
2022-02-07 15:11:46 -0300

    Board and Piece

--- src/apllication/Program.java
+++ src/apllication/Program.java
@@ -1,10 +1,10 @@
 package apllication;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Program {
     public static void main(String[] args) throws Exception {
-       Position pos = new Position(3, 5);
-       System.out.println(pos);
+
+        Board board = new Board(8, 8);
     }
 }
